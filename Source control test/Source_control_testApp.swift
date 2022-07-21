cafd6b51940a5e182814d2f48ee4350012e32692
Ela Murat <ela.murat@icloud.com>
2022-07-21 14:20:47 -0500

    Initial Commit

---
+++ Source control test/Source_control_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Source_control_testApp.swift
+//  Source control test
+//
+//  Created by Ela Murat on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Source_control_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
